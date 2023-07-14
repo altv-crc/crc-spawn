84cd3e47437743d58d515e207911b2e32d8440d7
Stuyk <stuykgaming@gmail.com>
2023-07-13 21:46:20 -0600

    Add Schema Support

---
+++ schemas/Character.d.ts
@@ -0,0 +1,7 @@
+import * as alt from 'alt-shared';
+
+declare module 'alt-crc' {
+    interface Character {
+        pos?: alt.Vector3;
+    }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,10 @@
 import * as alt from 'alt-server';
-import * as shared from 'alt-shared';
 import * as crc from '@stuyk/cross-resource-cache';
+import { Appearance, Character } from 'alt-crc';
 
 const defaultSpawn = new alt.Vector3(0, 0, 72);
 
 crc.database.onReady(() => {});
-
-interface Character {
-    _id: string;
-    pos: alt.Vector3;
-    appearance: shared.Appearance;
-}
 
 crc.events.onSync(async (entity: alt.Entity) => {
     if (!(entity instanceof alt.Player)) {
@@ -18,7 +12,7 @@ crc.events.onSync(async (entity: alt.Entity) => {
     }
 
     alt.logDebug('crc-spawn | Synchronized Character... Spawning...');
-    alt.emit('crc-appearance-apply', entity, crc.data.getValue<shared.Appearance>(entity, 'appearance'));
+    alt.emit('crc-appearance-apply', entity, crc.data.getValue<Appearance>(entity, 'appearance'));
 
     const pos = crc.data.getValue<alt.Vector3>(entity, 'pos');
 
